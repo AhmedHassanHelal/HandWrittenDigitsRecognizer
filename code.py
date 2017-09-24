67374bd764ee784f017bc58b76705784ff398f7b
AhmedHassanHelal <ahmedhassan96@windowslive.com>
2017-09-25 01:19:43 +0200

    Update code.py

--- code.py
+++ code.py
@@ -4,14 +4,35 @@ import random
 
 mndata = MNIST('/home/ahmed/MyWorkspace/Hand Writing Digits Recognizer/samples')
 
-images, labels = mndata.load_training()
+images_train_orig, labels_train_orig = mndata.load_training()
+images_test, labels_test = mndata.load_testing()
 
-X_train = np.asarray(images)
+X_train = np.asarray(images_train_orig)
+X_test = np.asarray(images_test)
 
 m_train = X_train.shape[0]
+
+m_test = X_test.shape[0]
 
 image_size = X_train.shape[1]
 
 index = random.randrange(0, len(images))  # choose an index ;-)
 
 print(mndata.display(images[index]))
+
+def sigmoid(z):
+    """
+    Compute the sigmoid of z
+
+    Arguments:
+    z -- A scalar or numpy array of any size.
+
+    Return:
+    s -- sigmoid(z)
+    """
+
+    ### START CODE HERE ### (≈ 1 line of code)
+    s = None
+    ### END CODE HERE ###
+    
+    return s
