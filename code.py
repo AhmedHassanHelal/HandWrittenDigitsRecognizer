ac90506a9703dff34d5835523add859f5a57ecda
AhmedHassanHelal <ahmedhassan96@windowslive.com>
2017-09-25 01:27:25 +0200

    Update code.py

--- code.py
+++ code.py
@@ -2,6 +2,8 @@ from mnist import MNIST
 import numpy as np
 import random
 
+NUM_DIGItS
+
 mndata = MNIST('/home/ahmed/MyWorkspace/Hand Writing Digits Recognizer/samples')
 
 images_train_orig, labels_train_orig = mndata.load_training()
@@ -33,3 +35,24 @@ def sigmoid(z):
     s = 1/(1+np.exp(-z))
     
     return s
+def initialize_with_zeros(dim):
+    """
+    This function creates a vector of zeros of shape (dim, 1) for w and initializes b to 0.
+    
+    Argument:
+    dim -- size of the w vector we want (or number of parameters in this case)
+    
+    Returns:
+    w -- initialized vector of shape (dim, 1)
+    b -- initialized scalar (corresponds to the bias)
+    """
+    
+    ### START CODE HERE ### (≈ 1 line of code)
+    w = np.zeros((dim, NUM_DIGItS))
+    b = np.zeros((NUM_DIGItS,1))
+    ### END CODE HERE ###
+
+    assert(w.shape == (dim, 1))
+    assert(isinstance(b, float) or isinstance(b, int))
+    
+    return w, b
