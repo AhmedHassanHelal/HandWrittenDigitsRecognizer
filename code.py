43c6bf14f930846404c471d6a63de80db00be419
AhmedHassanHelal <ahmedhassan96@windowslive.com>
2017-09-26 01:35:30 +0200

    Create code.py

--- code.py
+++ code.py
@@ -35,7 +35,7 @@ m_test = X_test.shape[1]
 
 image_size = X_train.shape[0]
 
-index = random.randrange(0, len(images_train_orig))  # choose an index ;-)
+index = random.randrange(0, len(images_train_orig))
 
 print(mndata.display(images_train_orig[index]))
 
@@ -48,22 +48,21 @@ def sigmoid(z):
 
 
 def initialize_with_zeros(dim):    
-    w = np.zeros((dim, NUM_DIGItS))
+    w = np.zeros((dim, NUM_DIGItS))#np.random.randn(dim, NUM_DIGItS)*0.001
     b = np.zeros((NUM_DIGItS,1))
     return w, b
 
 
 def propagate(w, b, X, Y):
     m = X.shape[1]
-    
     Z = np.dot(w.T, X)+b
     A = sigmoid(Z)
-    cost = (- 1 / m) * np.sum(Y * np.log(A) + (1 - Y) * (np.log(1 - A)), axis = 1)
-    print(cost.shape)
-    dw = (1 / m) * np.dot(X, (A - Y).T)
-    print(dw.shape)
-    db = (1 / m) * np.sum(A - Y)
-    print(db.shape)
+    
+    cost = ((- 1.0 / m) * np.sum(Y * np.log(A) + (1 - Y) * (np.log(1 - A)), axis = 1)).reshape(NUM_DIGItS, 1)
+    
+    dw = (1.0 / m) * np.dot(X, (A - Y).T)
+    
+    db = ((1.0 / m) * np.sum(A - Y, axis = 1) ).reshape(NUM_DIGItS, 1)
     
     cost = np.squeeze(cost)
     
@@ -80,7 +79,7 @@ def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = False):
         grads, cost = propagate(w, b, X, Y)
         dw = grads["dw"]
         db = grads["db"]
-        
+
         w = w - learning_rate * dw
         b = b - learning_rate * db
 
@@ -115,16 +114,17 @@ def predict(w, b, X):
 def model(X_train, Y_train, X_test, Y_test, num_iterations=2000, learning_rate=0.5, print_cost=False):
     w, b = initialize_with_zeros(X_train.shape[0])
 
-    parameters, grads, costs = optimize(w, b, X_train, Y_train, num_iterations, learning_rate, print_cost)
+    for i in range(num_iterations/20):
+    	parameters, grads, costs = optimize(w, b, X_train, Y_train, 20, learning_rate, print_cost)
     
-    w = parameters["w"]
-    b = parameters["b"]
+    	w = parameters["w"]
+    	b = parameters["b"]
     
-    Y_prediction_test = predict(w, b, X_test)
-    Y_prediction_train = predict(w, b, X_train)
-
-    print("train accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_train - Y_train)) * 100))
-    print("test accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_test - Y_test)) * 100))
+    	Y_prediction_test = predict(w, b, X_test)
+    	Y_prediction_train = predict(w, b, X_train)
+	print(num_iterations/20-i)
+    	print("train accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_train - Y_train)) * 100))
+    	print("test accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_test - Y_test)) * 100))
 
     
     d = {"costs": costs,
@@ -137,28 +137,9 @@ def model(X_train, Y_train, X_test, Y_test, num_iterations=2000, learning_rate=0
     
     return d
 
+d = model(X_train, Y_Expected_Train, X_test, Y_Expected_Test, num_iterations = 600, learning_rate = 0.00001, print_cost = False)
 
 
-
-learning_rates = [0.01, 0.001, 0.0001]
-models = {}
-for i in learning_rates:
-    print ("learning rate is: " + str(i))
-    models[str(i)] = model(X_train, Y_Expected_Train, X_test, Y_Expected_Test, num_iterations = 1500, learning_rate = i, print_cost = False)
-    print ('\n' + "-------------------------------------------------------" + '\n')
-
-for i in learning_rates:
-    plt.plot(np.squeeze(models[str(i)]["costs"]), label= str(models[str(i)]["learning_rate"]))
-
-plt.ylabel('cost')
-plt.xlabel('iterations')
-
-legend = plt.legend(loc='upper center', shadow=True)
-frame = legend.get_frame()
-frame.set_facecolor('0.90')
-plt.show()
-
-
-my_image = X_train[:, index]
+my_image = (X_train[:, index]).reshape(X_train.shape[0], 1)
 my_predicted_image = predict(d["w"], d["b"], my_image)
 print("y = " + str(np.squeeze(my_predicted_image)))
