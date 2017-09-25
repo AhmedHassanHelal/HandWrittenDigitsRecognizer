af9119558b40981c2a5a0777fef5fc1379215773
AhmedHassanHelal <ahmedhassan96@windowslive.com>
2017-09-25 20:52:35 +0200

    Update code.py

--- code.py
+++ code.py
@@ -7,53 +7,34 @@ NUM_DIGItS=10
 mndata = MNIST('/home/ahmed/MyWorkspace/Hand Writing Digits Recognizer/samples')
 
 images_train_orig, labels_train_orig = mndata.load_training()
-images_test, labels_test = mndata.load_testing()
+images_test, labels_test_orig = mndata.load_testing()
 
-X_train = np.asarray(images_train_orig)
-X_test = np.asarray(images_test)
+X_train = np.asarray(images_train_orig).T
+print(X_train.shape)
+Y_ORIG_train = np.asarray(labels_train_orig)
+Y_ORIG_train = Y_ORIG_train.reshape((Y_ORIG_train.shape[0], 1))
+X_test = np.asarray(images_test).T
+Y_ORIG_test = np.asarray(labels_test_orig)
+Y_ORIG_test = Y_ORIG_test.reshape((Y_ORIG_test.shape[0], 1))
 
-m_train = X_train.shape[0]
+m_train = X_train.shape[1]
 
-m_test = X_test.shape[0]
+m_test = X_test.shape[1]
 
-image_size = X_train.shape[1]
+image_size = X_train.shape[0]
 
-index = random.randrange(0, len(images))  # choose an index ;-)
+index = random.randrange(0, len(images_train_orig))  # choose an index ;-)
 
-print(mndata.display(images[index]))
+print(mndata.display(images_train_orig[index]))
 
 def sigmoid(z):
-    """
-    Compute the sigmoid of z
-
-    Arguments:
-    z -- A scalar or numpy array of any size.
-
-    Return:
-    s -- sigmoid(z)
-    """ 
     s = 1/(1+np.exp(-z))
-    
     return s
-def initialize_with_zeros(dim):
-    """
-    This function creates a vector of zeros of shape (dim, 10) for w and initializes b to 0.
-    
-    Argument:
-    dim --  number of parameters in this case
-    
-    Returns:
-    w -- initialized vector of shape (dim, 1)
-    b -- initialized scalar (corresponds to the bias)
-    """
-    
-    
+
+def initialize_with_zeros(dim):    
     w = np.zeros((dim, NUM_DIGItS))
     b = np.zeros((NUM_DIGItS,1))
-    
     return w, b
-
-
 
 def propagate(w, b, X, Y):
     """
@@ -79,8 +60,8 @@ def propagate(w, b, X, Y):
     # FORWARD PROPAGATION (FROM X TO COST)
     ### START CODE HERE ### (≈ 2 lines of code)
     Z = np.dot(w.T, X)+b
-    A = sigmoid(Z)                                    # compute activation
-    cost = None                                 # compute cost
+    A = sigmoid(Z)                            	        # compute activation
+    cost = None                                		# compute cost
     ### END CODE HERE ###
     
     # BACKWARD PROPAGATION (TO FIND GRAD)
@@ -95,3 +76,6 @@ def propagate(w, b, X, Y):
              "db": db}
     
     return grads, cost
+
+
+
