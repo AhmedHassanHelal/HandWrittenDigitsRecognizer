30624ff97e048700646a2b289138a932807dfb0c
AhmedHassanHelal <ahmedhassan96@windowslive.com>
2017-09-30 02:47:45 +0200

    Update code.py

--- code.py
+++ code.py
@@ -29,15 +29,15 @@ for i in range(NUM_DIGItS):
     Y_Expected_Test[i,:] = np.isin(Y_ORIG_test, [i]).astype(int)
 
 
-m_train = X_train.shape[1]
+#m_train = X_train.shape[1]
 
-m_test = X_test.shape[1]
+#m_test = X_test.shape[1]
 
-image_size = X_train.shape[0]
+#image_size = X_train.shape[0]
 
-index = random.randrange(0, len(images_train_orig))
+index = random.randrange(0, len(images_test))
 
-print(mndata.display(images_train_orig[index]))
+print(mndata.display(images_test[index]))
 
 
 
@@ -112,34 +112,41 @@ def predict(w, b, X):
 
 
 def model(X_train, Y_train, X_test, Y_test, num_iterations=2000, learning_rate=0.5, print_cost=False):
-    w, b = initialize_with_zeros(X_train.shape[0])
+	w, b = initialize_with_zeros(X_train.shape[0])
 
-    for i in range(num_iterations/20):
-    	parameters, grads, costs = optimize(w, b, X_train, Y_train, 20, learning_rate, print_cost)
-    
-    	w = parameters["w"]
-    	b = parameters["b"]
-    
-    	Y_prediction_test = predict(w, b, X_test)
-    	Y_prediction_train = predict(w, b, X_train)
-	print(num_iterations/20-i)
-    	print("train accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_train - Y_train)) * 100))
-    	print("test accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_test - Y_test)) * 100))
+	for i in range(num_iterations/20):
+		parameters, grads, costs = optimize(w, b, X_train, Y_train, 20, learning_rate, print_cost)
 
+		w = parameters["w"]
+		b = parameters["b"]
     
-    d = {"costs": costs,
+		Y_prediction_test = predict(w, b, X_test)
+		Y_prediction_train = predict(w, b, X_train)
+		print(num_iterations/20-i)
+		print("train accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_train - Y_train)) * 100))
+		print("test accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_test - Y_test)) * 100))
+
+
+	d = {"costs": costs,
          "Y_prediction_test": Y_prediction_test, 
          "Y_prediction_train" : Y_prediction_train, 
          "w" : w, 
          "b" : b,
          "learning_rate" : learning_rate,
          "num_iterations": num_iterations}
-    
-    return d
+	
+	return d
 
-d = model(X_train, Y_Expected_Train, X_test, Y_Expected_Test, num_iterations = 600, learning_rate = 0.00001, print_cost = False)
+d = model(X_train, Y_Expected_Train, X_test, Y_Expected_Test, num_iterations = 40, learning_rate = 0.00001, print_cost = False)
 
 
-my_image = (X_train[:, index]).reshape(X_train.shape[0], 1)
+my_image = (X_test[:, index]).reshape(X_test.shape[0], 1)
 my_predicted_image = predict(d["w"], d["b"], my_image)
 print("y = " + str(np.squeeze(my_predicted_image)))
+
+for i in range(NUM_DIGItS):
+	if(my_predicted_image[i] == 1):
+		print('The number you wrote: ' + str(i))
+
+values_of_w = open("values_of_w.txt", "w")
+values_of_w.write(str(d["w"]))
